c88b70cf79596634f6f487d44687ba0c12918ce5
Kade Boltjes <kboltjes@purdue.edu>
2021-02-24 19:19:06 -0500

    Created a unit test called FirebaseTests.FirebaseRegistrationTest that verifies user accounts can be created.

--- VirtuCardHost/Assets/Scripts/FirebaseScripts/User.cs
+++ VirtuCardHost/Assets/Scripts/FirebaseScripts/User.cs
@@ -16,6 +16,42 @@ namespace FirebaseScripts
             AuthStateChanged(null, null);
         }
 
+        /// <summary>
+        /// This method creates a TEMPORARY testing account. It just creates the account and then immediately deletes it for testing purposes.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="callback"></param>
+        public static void RegisterTestingAccount(String email, String password, Action<bool> callback)
+        {
+            auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    //Throw error for cancellation here 
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                    callback(false);
+                    return;
+                }
+
+                if (task.IsFaulted)
+                {
+                    //Throw error for other error here
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    callback(false);
+                    return;
+                }
+
+                // Firebase user has been created.
+                user = task.Result;
+                //Put callback here to return to when done. 
+                callback(true);
+                Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                    user.DisplayName, user.UserId);
+                user.DeleteAsync();
+            });
+        }
+
         public static void RegisterAccount(String email, String password, Action<bool> callback)
         {
             auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
