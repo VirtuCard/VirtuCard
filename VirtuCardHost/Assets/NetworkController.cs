fbb48af6b4116acb2a994cd896e1951105162655
umangsharma24 <sharm390@purdue.edu>
2021-02-28 18:23:15 -0500

    Created RoomCodeTest & Updated NetworkController.cs
    Test for the RoomCode is executed via a BASH script in the Assets Directory.
    The script is titled 'RoomCodeTest' and checks the RoomCode.txt file's contents to see if the RoomCode is appropriate.
    
    The NetworkController.cs file was updated - specifically the Start() method - in order to output the generated Room Code into the file RoomCode.txt.

--- VirtuCardHost/Assets/NetworkController.cs
+++ VirtuCardHost/Assets/NetworkController.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.IO;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
     private int RoomCodeLength = 6;
-    //Field for Host's RoomCode. In the format of ABCDEF
-    public string RoomCode = "";
+    //Field for Host's RoomCode.
+    //Format: ABCDEF
+    private string RoomCode = "";
+    //Field to store the name of the file
+    // that holds the RoomCode for testing purposes
+    private string RoomCodeFileName = "RoomCode.txt";
 
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings(); //Connects to Photon Master Servers
         generateCode();
+        WriteRoomCodeToFile();
     }
 
     public override void OnConnectedToMaster()
@@ -31,24 +37,31 @@ public class NetworkController : MonoBehaviourPunCallbacks
         // corresponds to a letter. For example, 0 is A.
         for (int i = 0; i < RoomCodeLength; i++)
         {
-            RoomCodeNum[i] = Random.Range(0, 25);
+            RoomCodeNum[i] = Random.Range(65, 90);
         }
         //Coverting Numbers into ASCII and then appending them to
         // roomCodeBuffer
         string roomCodeBuffer = "";
         for (int i = 0; i < RoomCodeLength; i++)
         {
-            roomCodeBuffer += (char)(RoomCodeNum[i] + 65);
+            roomCodeBuffer += (char)(RoomCodeNum[i]);
         }
         //Setting Host's RoomCode field to generated roomCodeBuffer
         RoomCode = roomCodeBuffer;
     }
 
-
     // Function to Create and Join a Room with associated Room Code
     void CreateAndJoinRoom()
     {
         PhotonNetwork.CreateRoom(RoomCode);
+    }
+
+    /// WriteRoomCodeToFile()
+    /// 
+    /// Function used to write the Room Code to a File
+    void WriteRoomCodeToFile()
+    {
+        File.WriteAllText(RoomCodeFileName, RoomCode);
     }
 
 }
