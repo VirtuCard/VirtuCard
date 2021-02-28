3da966f7b18eab0b2f7aeaeda5e5bc1c2208fc3e
umangsharma24 <sharm390@purdue.edu>
2021-02-27 19:17:21 -0500

    Implemented RoomCode Generating Function
    Implemented Function to generate RoomCode in NetworkController.cs
    
    Function generates 6 random numbers, converts each to an ASCII table value (through arithmetic), and casts each number to a letter.

--- VirtuCardHost/Assets/NetworkController.cs
+++ VirtuCardHost/Assets/NetworkController.cs
@@ -5,15 +5,23 @@ using Photon.Pun;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
+
+    private int RoomCodeLength = 6;
+    //Field for Host's RoomCode. In the format of ABCDEF
+    public string RoomCode = "";
+
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings(); //Connects to Photon Master Servers
+        generateCode();
+        
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("The Host is now connected to the " + PhotonNetwork.CloudRegion + " server.");
+        Debug.Log("Generated Room Code is " + RoomCode);
     }
 
     // Update is called once per frame
@@ -21,4 +29,35 @@ public class NetworkController : MonoBehaviourPunCallbacks
     {
         
     }
+
+    // Generate Room Code Function
+    void generateCode()
+    {
+        //Integer Array used to create RoomCode
+        int[] RoomCodeNum = new int[RoomCodeLength];
+        //Placing Numbers to act as 'pseudo' letters. Each number
+        // corresponds to a letter. For example, 0 is A.
+        for (int i = 0; i < RoomCodeLength; i++)
+        {
+            RoomCodeNum[i] = Random.Range(0, 25);
+        }
+        //Coverting Numbers into ASCII and then appending them to
+        // roomCodeBuffer
+        string roomCodeBuffer = "";
+        for (int i = 0; i < RoomCodeLength; i++)
+        {
+            roomCodeBuffer += (char) (RoomCodeNum[i] + 65);
+        }
+        //Setting Host's RoomCode field to generated roomCodeBuffer
+        RoomCode = roomCodeBuffer;
+    }
+
+ 
+    // Function to Create and Join a Room with associated Room Code
+    void CreateAndJoinRoom()
+    {
+        
+        PhotonNetwork.CreateRoom(RoomCode);
+    }
+
 }
