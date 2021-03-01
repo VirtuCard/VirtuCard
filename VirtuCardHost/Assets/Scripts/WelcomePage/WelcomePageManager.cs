8f12d096578dbfecc3f6e2b4b35c77983253de03
JuneSeo27 <seo83@purdue.edu>
2021-03-01 01:48:17 -0500

    doing this to merge

--- VirtuCardHost/Assets/Scripts/WelcomePage/WelcomePageManager.cs
+++ VirtuCardHost/Assets/Scripts/WelcomePage/WelcomePageManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class WelcomePageManager : MonoBehaviour
 {
     // these are the text boxes for the username and password inputs
