df4d034ed1b0761dd5584a4434212323d713e649
JuneSeo27 <seo83@purdue.edu>
2021-03-01 02:22:35 -0500

    goodnight

--- VirtuCardHost/Assets/Scripts/LandingPage/LandingPageManager.cs
+++ VirtuCardHost/Assets/Scripts/LandingPage/LandingPageManager.cs
@@ -2,7 +2,6 @@ using FirebaseScripts;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class LandingPageManager : MonoBehaviour
 {
     // this dropdown should be populated with all the game names
@@ -27,33 +26,45 @@ public class LandingPageManager : MonoBehaviour
         //Console.WriteLine("HELLLLLOOOOO");
 
 
-        /*
-        FirebaseInit.InitializeFirebase(task =>
-        {
-            AuthUser.RegisterAccount("testing", "hello@ohio.edu", "topsecret",
-                task => { Debug.Log("Hi " + task); 
-                if (AuthUser.Login("testing", "topsecret")) {
-                        Console.WriteLine("True");
-                    }
-                    else {
-                        Console.WriteLine("False");
-                    }
-                });
-        });
+        
+        // FirebaseInit.InitializeFirebase(task =>
+        // {
+        //     AuthUser.RegisterAccount("testing", "hello@ohio.edu", "topsecret",
+        //         task => { Debug.Log("Hi " + task); 
+        //         if (AuthUser.Login("testing", "topsecret")) {
+        //                 Debug.Log("True");
+        //             }
+        //             else {
+        //                 Debug.Log("False");
+        //             }
+        //         });
+        // });
 
         FirebaseInit.InitializeFirebase(task =>
         {
-            AuthUser.RegisterAccount("juniebear", "seodongjune00@gmail.com", "chicken",
-                task => { 
-                    if (AuthUser.Login("juniebear27", "chicken")) {
-                        Console.WriteLine("True");
-                    }
-                    else {
-                        Console.WriteLine("False");
-                    }
-                 });
+            bool check = AuthUser.Login("seodongjune00@gmail.com", "chicken");
+            if (check) {
+                Debug.Log("TRUE JUNIBEAR");
+                //Console.WriteLine("TRUE JUNIBEAR");
+            }
+            else {
+                Debug.Log("FALSE JUNIEBEAR poopie");
+                //Console.WriteLine("FALSE JUNIBEAR");
+
+            }
+
+            // AuthUser.RegisterAccount("juniebear", "seodongjune00@gmail.com", "chicken",
+            //     task => { 
+            //         Debug.Log("Checking juniebear");
+            //         if (AuthUser.Login("seodongjune00@gmail.com", "chicken")) {
+            //             Debug.Log("True");
+            //         }
+            //         else {
+            //             Debug.Log("False");
+            //         }
+            //      });
         });
-        */
+        
     }
 
     // Update is called once per frame
