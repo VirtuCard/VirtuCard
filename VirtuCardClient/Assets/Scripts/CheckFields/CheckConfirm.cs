2bc9ed196c4b3913e82385dce816baa6b917c25f
rhawks00 <54541700+rhawks00@users.noreply.github.com>
2021-03-02 01:41:48 -0500

    Added Email and Password Checking
    This is an early version of this feature. I will refine it and make it work better. The password field option on Unity makes it hard to look at the password as plain text rather than astericks.

---
+++ VirtuCardClient/Assets/Scripts/CheckFields/CheckConfirm.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+public class CheckConfirm : MonoBehaviour
+{
+   public string toCheck;
+   public string confirmField;
+   public GameObject toCheckField;
+   public GameObject inputField;
+   public GameObject textDisplay;
+   
+   public void updateText()
+   {
+       toCheck = toCheckField.GetComponent<Text>().text;
+       confirmField = inputField.GetComponent<Text>().text;
+
+       if (toCheck != confirmField)
+       {
+         textDisplay.GetComponent<Text>().text = "Make sure your passwords match!";
+       }
+       else
+       {
+          textDisplay.GetComponent<Text>().text = "";
+       }
+       if (toCheck == "")
+       {
+          textDisplay.GetComponent<Text>().text = "";
+       }
+   }
+}
