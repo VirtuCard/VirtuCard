a773f6d5548f6cfc934d0ca6555f6db05fccc76c
Kade Boltjes <kboltjes@purdue.edu>
2021-03-04 20:53:39 -0500

    Created skip turn button for client side and created ClientData static class to store all the client-side game data

---
+++ VirtuCardClient/Assets/Scripts/Game/ClientData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ClientData
+{
+    private static bool isCurrentlyTurn = false;
+
+    public static void setCurrentTurn(bool value)
+    {
+        isCurrentlyTurn = value;
+    }
+    public static bool isCurrentTurn()
+    {
+        return isCurrentlyTurn;
+    }
+}
