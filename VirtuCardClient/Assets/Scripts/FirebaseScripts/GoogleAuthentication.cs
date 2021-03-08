b79bafe67bead625cd066a239b004dc0030c25e2
umangsharma24 <sharm390@purdue.edu>
2021-03-07 20:26:53 -0500

    Generated SHA1 value for Google Authentication
    Generated SHA1 value for Google Authentication. The Keystore value was encrypted using the password saved in the Emails/Password Document for the organization.
    
    Changed build settings for Client from PC to Android.

---
+++ VirtuCardClient/Assets/Scripts/FirebaseScripts/GoogleAuthentication.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google;
+using Firebase;
+using FirebaseScripts;
+
+public class GoogleAuthentication : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+    }
+
+    /// OnButtonClick()
+    /// 
+    /// Function to bring Google Sign-In UI.
+    void OnButtonClick()
+    {
+        
+    }
+}
