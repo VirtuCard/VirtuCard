1f361e9ec48414262aa90a3b9b895b427afae31b
rhawks00 <54541700+rhawks00@users.noreply.github.com>
2021-03-08 12:43:34 -0500

    Added Join Game Error
    For local testing purposes there is a string preset to check if the code is valid or not. Eventually this will be using Photon.

--- VirtuCardClient/Assets/Scripts/JoinGamePage/JoinGameMethod.cs
+++ VirtuCardClient/Assets/Scripts/JoinGamePage/JoinGameMethod.cs
@@ -11,14 +11,38 @@ namespace JoinGamePage
         /// This method grabs the code that the user input
         /// then puts it in to the code to join a Photon room.
         /// <summary>
-
+       
         public string joinCode;
         public GameObject inputField;
+        public GameObject textDisplay;
+
+        // This string is for testing purposes, eventually we will use Photon to 
+        // connect the user and see if the code input is valid
+        public string testCode = "123456";
 
         public void ConnectClientClicked()
         {
             joinCode = inputField.GetComponent<Text>().text;
-            Debug.Log("Join Code is: " + joinCode);
+            //Debug.Log("Join Code is: " + joinCode);
+
+            if (joinCode == testCode)
+            {
+                // for local testing on the sprint review this is what we can do
+                // this will change the scene to the waiting screen in here
+
+                Debug.Log("Successful code entered.");
+                textDisplay.GetComponent<Text>().text = "";
+            }
+            else
+            {
+                Debug.Log("Incorrect code");
+                textDisplay.GetComponent<Text>().text = "Please input a valid code.";
+            }
+
+            if (joinCode == "")
+            {
+                textDisplay.GetComponent<Text>().text = "";
+            }
   
         }
     }
