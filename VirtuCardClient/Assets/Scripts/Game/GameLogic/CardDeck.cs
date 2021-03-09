e5a6d31ec746ea3fd1c742307b48315c45d86b7c
Kade Boltjes <kboltjes@purdue.edu>
2021-03-08 19:19:34 -0500

    Added host-end functionality for when a player requests to draw cards in the GameScreenController class.

--- VirtuCardClient/Assets/Scripts/Game/GameLogic/CardDeck.cs
+++ VirtuCardClient/Assets/Scripts/Game/GameLogic/CardDeck.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CardDeck
 {
-    List<Card> cards = new List<Card>();
+    private List<Card> cards = new List<Card>();
 
     /// <summary>
     /// This prints all the cards in the CardDeck by calling each one's overloaded print method
@@ -41,6 +41,45 @@ public class CardDeck
     public void AddCard(Card card)
     {
         cards.Add(card);
+    }
+
+    /// <summary>
+    /// Gets a random card from the deck and removes it.
+    /// That card is then returned
+    /// </summary>
+    /// <returns></returns>
+    public Card PopCard()
+    {
+        int count = cards.Count;
+        System.Random rand = new System.Random();
+        int cardIndex = rand.Next(0, count - 1);
+
+        Card returnCard = GetCard(cardIndex);
+        RemoveCard(cardIndex);
+        return returnCard;
+    }
+
+    /// <summary>
+    /// Returns the card residing at a particular index in the card deck
+    /// </summary>
+    /// <param name="index">0-indexed location of the card</param>
+    /// <returns></returns>
+    public Card GetCard(int index)
+    {
+        if (index < cards.Count && index >= 0)
+        {
+            return cards[index];
+        }
+        throw new System.Exception("Card Deck of size: " + cards.Count + " does not contain card at index: " + index);
+    }
+
+    /// <summary>
+    /// Returns the number of cards in the deck
+    /// </summary>
+    /// <returns></returns>
+    public int GetCardCount()
+    {
+        return cards.Count;
     }
 
     /// <summary>
--- VirtuCardHost/Assets/Scripts/GameScreen/GameScreenController.cs
+++ VirtuCardHost/Assets/Scripts/GameScreen/GameScreenController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class GameScreenController : MonoBehaviour
 {
     public GameObject chatPanel;
     public Toggle chatToggle;
+
+    // the game being played
+    private Game game;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +25,31 @@ public class GameScreenController : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// This method should be called when a client has requested to draw a single card
+    /// It draws a random card from the undealt deck and then removes it.
+    /// </summary>
+    /// <returns></returns>
+    public Card DrawCard()
+    {
+        return game.DrawCardFromDeck(DeckChoices.UNDEALT);
+    }
+
+    /// <summary>
+    /// This method should be called when a client has requested to draw multiple cards.
+    /// It draws <paramref name="numOfCards"/> random cards from the undealt deck and subsequently removes them.
+    /// </summary>
+    /// <param name="numOfCards">Number of cards to draw and return</param>
+    /// <returns>A list containing all the drawn cards</returns>
+    public List<Card> DrawCards(int numOfCards)
+    {
+        return game.DrawCardsFromDeck(numOfCards, DeckChoices.UNDEALT);
+    }
+
+    /// <summary>
+    /// This method is called when the chat toggle state changes
+    /// </summary>
+    /// <param name="toggleVal"></param>
     private void ChatToggleValueChanged(bool toggleVal)
     {
         chatPanel.SetActive(!toggleVal);
