5a2f4cc6808631048f95412dc8279c46c2992ab6
Kade Boltjes <kboltjes@purdue.edu>
2021-03-10 16:27:44 -0500

    Linked the Selected game to the UI and put the current game in the HostData class. Attempted to create callback for OnPlayerEnteredRoom

--- VirtuCardHost/Assets/Scripts/GameScreen/GameLogic/PlayerInfo.cs
+++ VirtuCardHost/Assets/Scripts/GameScreen/GameLogic/PlayerInfo.cs
@@ -6,4 +6,10 @@ public struct PlayerInfo
 {
     public string username;
     public int score;
+    public Photon.Realtime.Player photonPlayer;
+
+    public override string ToString()
+    {
+        return "Name: \"" + username + "\" Score: " +  score;
+    }
 }
