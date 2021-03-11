e4fd943f2cbf1486c05f8f815ff41f1f1f18c09e
rhawks00 <54541700+rhawks00@users.noreply.github.com>
2021-03-10 21:11:08 -0500

    Added Checking Host functionality
    The game checks if the host is allowed to join their own game. If they aren't, it disconnects them.

--- VirtuCardClient/Assets/Scripts/JoinGamePage/JoinGameMethod.cs
+++ VirtuCardClient/Assets/Scripts/JoinGamePage/JoinGameMethod.cs
@@ -99,16 +99,23 @@ public class JoinGameMethod : MonoBehaviourPunCallbacks
             string s = (string) data[0];
             bool test = (bool) data[1];
             int players = (int) data[2];
+            string hostName = (string) data[3];
             Debug.Log(s);
             Debug.Log(test);
             Debug.Log(players);
+            Debug.Log(hostName);
 
             MaxPlayersText.GetComponent<Text>().text = s;
             GameModeText.GetComponent<Text>().text = "Max Players: " + players;
 
-            if (!test)
+            string clientName = PhotonNetwork.NickName;
+
+            if (test == false && clientName == hostName)
             {
-                errorCode.GetComponent<Text>().text = "Host cannot join!";
+                Debug.Log("Host is not allowed to join the game!");
+                //errorCode.GetComponent<Text>().text = "Host cannot join!";
+                PhotonNetwork.LeaveRoom();
+                SceneManager.LoadScene(SceneNames.JoinGamePage);
             }
         }
     }
