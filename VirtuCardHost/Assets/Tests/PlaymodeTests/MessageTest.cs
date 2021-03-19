2c734af435d7683c2ea4c06a6755b1599bce66d2
JuneSeo27 <seo83@purdue.edu>
2021-03-18 23:11:25 -0400

    finished unit test for message

--- VirtuCardHost/Assets/Tests/PlaymodeTests/MessageTest.cs
+++ VirtuCardHost/Assets/Tests/PlaymodeTests/MessageTest.cs
@@ -1,34 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
-using System.Text;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using FirebaseScripts;
+using Photon.Chat;
+using Photon.Pun;
+using PhotonScripts;
+using GameScreen.ChatPanel;
 
 public class MessageTest
 {
-    public string message = "junieseojunieseojunieseojunieseojunieseojunieseo";
-
     [UnityTest]
     public IEnumerator MessageTestWithEnumeratorPasses()
     {
         // checks if the message puts ... after 44 characters
-        ChatPanelController.SetText(message);
-        string edited = "junieseojunieseojunieseojunieseojunieseoj...";
+        string message = "junieseojunieseojunieseojunieseojunieseojunieseo";
+        ChatPanelController p = new ChatPanelController();
+        string edited = p.setAndGetTextTest(message);
+        string expected = "junieseojunieseojunieseojunieseojunieseoj...";
         bool check = false;
-        if (String.Compare(ChatPanelController.GetText(), edited) == 0) {
+        if (String.Compare(expected, edited) == 0) {
             check = true;
         }
         yield return new WaitForSeconds(2);
         Assert.IsTrue(check);
 
         // checks if the message has been sent
-        ChatPanelController.CreateNewMessage("Testing123", "UsernameTesting");
+        p.CreateNewMessage("Testing123", "UsernameTesting");
         yield return new WaitForSeconds(2);
-        Assert.IsTrue(ChatPanelController.getMessageCount() == 1);
-
+        Assert.IsTrue(p.getMessageCount() == 1);
 
         yield return new WaitForSeconds(2);;
     }
