ae838a362923717db628b8eacf05f5de3b45bd2f
JuneSeo27 <seo83@purdue.edu>
2021-03-20 16:26:59 -0400

    Made Dropdown box for the game scene
    Needs help cleaning up. deleting the button and stuff

--- VirtuCardHost/Assets/Scripts/HostData.cs
+++ VirtuCardHost/Assets/Scripts/HostData.cs
@@ -150,6 +150,7 @@ public static class HostData
     {
         return chatAllowed;
     }
+    
     public static void setChatAllowed(bool isChatAllowed)
     {
         chatAllowed = isChatAllowed;
