68abe90422b7f7c7b9a86c1c0b679582155a5c6c
JuneSeo27 <seo83@purdue.edu>
2021-03-24 20:39:01 -0400

    Started on PrivMessage

--- VirtuCardClient/Assets/Scripts/Game/ClientGameController.cs
+++ VirtuCardClient/Assets/Scripts/Game/ClientGameController.cs
@@ -16,7 +16,7 @@ public class ClientGameController : MonoBehaviourPunCallbacks
     public Button playCardBtn;
     public Button drawCardBtn;
     public GameObject errorDisplay;
-    public GameObject turn;
+    // public GameObject turn;
     public GameObject notTurnUI;
     public Text waitingSign;
 
@@ -30,6 +30,8 @@ public class ClientGameController : MonoBehaviourPunCallbacks
     public GameObject dropboxUI;
     public RectTransform dropboxSize;
     public GameObject chatPanel;
+    public Dropdown privChatOption;
+    public RectTransform privChatSize;
 
     // 3 below are used for if the game is over
     public GameObject winnerPanel;
@@ -99,6 +101,20 @@ public class ClientGameController : MonoBehaviourPunCallbacks
             goFishPanel.SetActive(false);
         }
 
+        // private message UI
+        privChatSize.offsetMin = new Vector2(privChatSize.offsetMin.x, 995);
+        privChatSize.offsetMax = new Vector2(privChatSize.offsetMax.x, 1085);
+        privChatOption.options.Add(new Dropdown.OptionData("Public chat")); // default
+        List<string> privName = ClientData.GetAllConnectedPlayers();
+        foreach (string namePlayer in privName)
+        {
+            // only add the name if it is not this person
+            if (!namePlayer.Equals(PhotonNetwork.NickName))
+            {
+                privChatOption.options.Add(new Dropdown.OptionData(namePlayer));
+            }
+        }
+
         // when winner is announced the button is clicked
         exitGameBtn.onClick.AddListener(delegate() { exitGameBtnOnClick(); });
     }
@@ -127,7 +143,7 @@ public class ClientGameController : MonoBehaviourPunCallbacks
                 SetupTurn();
             }
 
-            turn.SetActive(true);
+            // turn.SetActive(true);
             notTurnUI.SetActive(false);
 
 
@@ -163,7 +179,7 @@ public class ClientGameController : MonoBehaviourPunCallbacks
 
             wasCurrentlyTurn = false;
 
-            turn.SetActive(false);
+            // turn.SetActive(false);
             // Call the Username of the current player here
             waitingSign.GetComponent<Text>().text = ClientData.getCurrentPlayerTurn() + "'s Turn";
             if (String.IsNullOrEmpty(ClientData.getCurrentPlayerTurn()))
