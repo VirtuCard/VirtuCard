1cb067aa4fed2b67bae88b17687f1e4ff63cbf5b
Shayne Marques <marques0@purdue.edu>
2021-03-26 20:22:26 -0400

    Added script and error panel

---
+++ VirtuCardClient/Assets/Scripts/ProfileScreen/ProfileScreenController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileScreenController : MonoBehaviour
+{
+    public Text nameText;
+    public Text usernameText;
+    public Text emailText;
+    public Text gamesPText;
+    public Text gamesWText;
+    public Text gamesLText;
+    public Image avatarImage;
+    public Button editButton;
+    public GameObject errorPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
