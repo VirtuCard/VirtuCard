913d804b82046fd91f573b36280d91bb6abf8272
Shayne Marques <marques0@purdue.edu>
2021-04-10 16:51:26 -0400

    Added upload file button for current directory

--- VirtuCardHost/Assets/Scripts/FileUpload/BackgroundUploadPageManager.cs
+++ VirtuCardHost/Assets/Scripts/FileUpload/BackgroundUploadPageManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
 
 public class BackgroundUploadPageManager : MonoBehaviour
 {
+    public Button bgUploadBtn;
+
+    public void onUploadButtonClicked()
+    {
+        string path = EditorUtility.OpenFilePanel("Hi", "", "jpg");
+    }
     // Start is called before the first frame update
     void Start()
     {
