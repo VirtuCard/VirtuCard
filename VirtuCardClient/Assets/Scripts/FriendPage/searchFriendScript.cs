3375365abf01765c1ffd30638ac134149eb429d1
JuneSeo27 <seo83@purdue.edu>
2021-04-19 01:42:44 -0400

    Friends Search
    Can't search up a blank username, cannot add the person if you're already friends, fix little bugs

--- VirtuCardClient/Assets/Scripts/FriendPage/searchFriendScript.cs
+++ VirtuCardClient/Assets/Scripts/FriendPage/searchFriendScript.cs
@@ -13,13 +13,27 @@ public class searchFriendScript : MonoBehaviour
     public InputField username;
     public GameObject searchPanel;
     public string usernamePlayer;
+    public Text postSearchUser;
+    public Text addedOrAlreadyFriends;
+    public GameObject searchList;
+    public Button addBtn;
+    public GameObject addBtnObject;
+    public CanvasGroup alreadyFriendSign;
+    private User user;
+    private List<User> friends = new List<User>();
 
     // Start is called before the first frame update
     void Start()
     {
         searchIcon.onClick.AddListener(delegate { searchPanel.SetActive(true); });
-        backSearch.onClick.AddListener(delegate { searchPanel.SetActive(false); });
-        searchBtn.onClick.AddListener(delegate { searchingFriend(); });
+        backSearch.onClick.AddListener(delegate { backBtnPressed(); });
+        searchBtn.onClick.AddListener(delegate { 
+                if (username.text != "" )
+                {
+                    searchingFriend();
+                }
+            });
+        addBtn.onClick.AddListener(delegate { addBtnPressed(); });
 
     }
 
@@ -54,16 +68,81 @@ public class searchFriendScript : MonoBehaviour
 
         Debug.Log("final test " + usernamePlayer);
 
+        searchList.SetActive(true);
+        string searchResult;
         // goes in if the user exists.
         if (!usernamePlayer.Equals("person does not exist"))
         {
+            addBtnObject.SetActive(false);
+            searchResult = usernameSearching + " does not exist.";
 
         }
         else 
         {
-
+            addBtnObject.SetActive(true);
+            searchResult = usernameSearching;
         }
 
+        postSearchUser.GetComponent<Text>().text = searchResult;
 
+    }
+
+    public void backBtnPressed()
+    {
+        username.text = "";
+        searchList.SetActive(false);
+        searchPanel.SetActive(false);
+    }
+
+    public void addBtnPressed()
+    {
+        user = ClientData.UserProfile;
+        string usernameSearching = username.text;
+        //List<string> currentFriends = new List<string>();
+
+        int alreadyFriends = 0;
+
+        // this checks if this is person is already friends
+        foreach (string friendName in user.Friends)
+        {
+            // currentFriends.Add(friendName);
+            if (friendName == usernameSearching)
+            {
+                alreadyFriends = 1;
+            }
+        }
+
+        if (alreadyFriends == 1) // this person is already your friend
+        {
+            Debug.Log("already friends");
+            addedOrAlreadyFriends.GetComponent<Text>().text = "You are already friends with this user";
+            alreadyFriendSign.GetComponent<CanvasGroup>().alpha = 1;
+            StartCoroutine(FadeCanvas(alreadyFriendSign, alreadyFriendSign.alpha, 0));
+        }
+        else // adding this person normally
+        {
+            addedOrAlreadyFriends.GetComponent<Text>().text = "You have added this user!";
+            alreadyFriendSign.GetComponent<CanvasGroup>().alpha = 1;
+            StartCoroutine(FadeCanvas(alreadyFriendSign, alreadyFriendSign.alpha, 0));
+
+        }
+    }
+
+    public IEnumerator FadeCanvas(CanvasGroup cg, float start, float end, float lerpTime = 1.0f)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+            cg.alpha = currentValue;
+            if (percentageComplete >= 1) break;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
